4194f3994b19932e25fb188cf03ceddd35554e4b
roseiaa <ljrobinson268@gmail.com>
2024-12-16 16:05:13 +0000

    allow cross origin resource sharing

---
+++ src/Components/Articles.jsx
@@ -0,0 +1,11 @@
+function Articles() {
+    return (
+        <main>
+        <h2>Hello!</h2>
+        
+        </main>
+    )
+}
+
+
+export default Articles
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,7 @@
+function Footer(){
+    return(
+        <p>Northcoders Copyright 2024</p>
+    )
+}
+
+export default Footer
