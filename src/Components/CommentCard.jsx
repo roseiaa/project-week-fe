e3ab3ce8d1b07fbb47bc539767583937926c6997
roseiaa <ljrobinson268@gmail.com>
2024-12-18 12:10:07 +0000

    added vote buttons

--- src/Components/CommentCard.jsx
+++ src/Components/CommentCard.jsx
@@ -1,16 +1,66 @@
-import axios from "axios"
-import { useState } from "react"
+import axios from "axios";
+import { useState } from "react";
 
-function CommentCard({comment}){
-    const [vote, setVotes] = useState(comment.votes)
-    return(
-         <>
-             <h3>{comment.author}</h3>
-             <p>{comment.created_at}</p>
-             <p className="comment-text">{comment.body}</p>
-             <p className="votes">Votes: {vote}</p>
-         </>
-    )
+function CommentCard({ comment }) {
+  const [vote, setVotes] = useState(comment.votes);
+  const voteTracker = {
+    inc_votes: 1,
+  };
+
+  function addVote() {
+    voteTracker.inc_votes = 1
+  }
+  function minusVote() {
+    voteTracker.inc_votes = -1
+  }
+
+  function modifyVotes() {
+    axios
+      .patch(
+        `https://nc-news-jstr.onrender.com/api/comments/${comment.comment_id}`,
+        voteTracker
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        alert(err)
+        console.log(err);
+      });
+  }
+
+  return (
+    <>
+      <h3>{comment.author}</h3>
+      <p>{comment.created_at}</p>
+      <p className="comment-text">{comment.body}</p>
+      <p className="votes">Votes: {vote}</p>
+      <button
+        className="like-button"
+        onClick={() => {
+          setVotes((votes) => {
+            return votes + 1;
+          });
+          addVote();
+          modifyVotes();
+        }}
+      >
+        Like
+      </button>
+      <button
+        className="dislike-button"
+        onClick={() => {
+          setVotes((votes) => {
+            return votes - 1;
+          });
+          minusVote();
+          modifyVotes();
+        }}
+      >
+        Dislike
+      </button>
+    </>
+  );
 }
 
-export default CommentCard
+export default CommentCard;
